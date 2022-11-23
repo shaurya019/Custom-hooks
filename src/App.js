bebacf10a96364f505334c587d165fbde3c9fc86
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2022-11-24 02:08:03 +0530

    custom-hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import DocTitleOne from './components/DocTitleOne'
+import CounterOne from './components/CounterOne'
+import UserForm from './components/UserForm'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<DocTitleOne />
+			<CounterOne />
+			<UserForm />
+		</div>
+	)
 }
 
-export default App;
+export default App
